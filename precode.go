dd342e19d5af7466870c71a1943fb3f7c88586a2
1lIMDiN <limdinhazerd@mail.ru>
2025-05-03 22:08:26 +1000

    add files

---
+++ precode.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+)
+
+
+func main() {
+	r := chi.NewRouter()
+
+
+
+
+	if err := http.ListenAndServe(":8080", r); err != nil {
+		fmt.Printf("Ошибка при запуске сервера: %s", err.Error())
+		return
+	}
+}
